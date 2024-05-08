846dbaf4dc9cc4fc84a444a114cb7b16e3d8021d
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-08 17:51:30 +0530

    some-message

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hii Ramesh, I am fine. How are you?"
+echo "Ramesh::"I am fine too. how is your work
+echo "Suresh:: not bad. I am thinking to your DevOps"
