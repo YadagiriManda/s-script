e676763ebe3b9a9e1c4ec1b74060e3f7af8ec67a
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 13:19:28 +0530

    some-message

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,11 +2,13 @@
 
 NUMBER=$1
 
-if [ $NUMBER -le 10 ]
+if [ $NUMBER -eq 10 ]
 then
     echo "Given number $NUMBER is greater than 10"
+ if else
+    echo "Given number $NUMBER is less than 10"
 else
-    echo "Given number $NUMBER is less than 10" 
+    echo "Given number $NUMBER is equal to 10" 
 fi
 
 #-gt, -lt, -eq, -ge, -le
