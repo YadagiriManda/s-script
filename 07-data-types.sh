39e1783f291803eed0b6143f0ac220f118356e23
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 12:48:24 +0530

    some-message

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -4,6 +4,8 @@ NO1=$1
 NO2=$2
 
 #SUM=$(($NO1+$NO2))
- SUB=$(($NO1-$NO2))
+ #SUB=$(($NO1-$NO2))
+ MUL=$(($NO1*$NO2))
 
-echo "Total of $NO1 and $NO2 is: $SUB"
+
+echo "Total of $NO1 and $NO2 is: $MUL"
