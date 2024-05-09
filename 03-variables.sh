8b0859c696b7a7d70379d5237a0532b0bb9ec26d
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 11:28:44 +0530

    some-message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# declaring the variables
+PERSON1 = Sachin
+PERSON2 = Rahul
+#reffering the variables
+echo "$PERSON1:: Hello $PERSON2,How are you?"
+echo "$PERSON2:: Hii $PERSON1, I am fine, How are you"
+echo "$PERSON1:: Iam fine too, How is your wirk"
+echo "$PERSON2:: Not bad, Iam thinking to upgrade to DevOps.."
