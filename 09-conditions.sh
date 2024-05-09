832678a0bf6be7785451687d462c955a695f47ef
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 13:24:51 +0530

    some-message

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,12 +3,13 @@
 NUMBER=$1
 
 if [ $NUMBER -eq 10 ]
+then
 
     echo "Given number $NUMBER is greater than 10"
 else if
     echo "Given number $NUMBER is less than 10"
-then
-    echo "Given number $NUMBER is equal to 10" 
+else if
+    echo "Given number $NUMBER is equals to 10"
 fi
 
 #-gt, -lt, -eq, -ge, -le
