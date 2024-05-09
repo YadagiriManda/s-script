969c05bb386e9fd06c129b33c08eddab2c060d7f
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 12:17:54 +0530

    some-message

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ echo "Please Enter Username::"
+
+ read USERNAME #here USERNAME is variable
+
+ echo "Please Enter Username::"
+
+ read PASSWORD
+
+ echo "Username is: $USERNAME, password is: $PASSWORD"
