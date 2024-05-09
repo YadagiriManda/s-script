deee6d179bac932360bdf246268b86851173571f
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 12:33:56 +0530

    some-message

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 MOVIES={"RRR","BAHUBALI","KGF"}
 
-echo "first movie is :: ${MOVIES[0]}"
-echo "second movie is :: ${MOVIES[1]}"
-echo "third movie is :: ${MOVIES[2]}"
+echo "first movie is : ${MOVIES[0]}"
+echo "second movie is : ${MOVIES[1]}"
+echo "third movie is : ${MOVIES[2]}"
