5c7587cc5135f7a1ba314f2dc9ea4ebfb9e2fcd7
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 11:58:23 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,5 +5,5 @@ PERSON2=Rahul
 #reffering the variables
 echo "$PERSON1:: Hello $PERSON2,How are you?"
 echo "$PERSON2:: Hii $PERSON1, I am fine, How are you"
-echo "$PERSON1:: Iam fine too, How is your wirk"
+echo "$PERSON1:: Iam fine too, How is your work?"
 echo "$PERSON2:: Not bad, Iam thinking to upgrade to DevOps.."
