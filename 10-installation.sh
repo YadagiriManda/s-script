e1018e3c15fef36fca9cc508adbdbce37757abfb
YadagiriManda <yadagiri4tech@gmail.com>
2024-05-09 14:07:24 +0530

    some-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,14 +2,14 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
-    echo "please run this script with root access."
-    exit 1 #manually exit if error comes.
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "you are super user."
+    echo "You are super user."
 fi
 
-dng insatll mysql -y
+dnf install mysql -y
 
-echo "is script proceeding"
+echo "is script proceeding."
